3f93c6130859fc04c405682bd6a983b3a138ba67
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-04-29 14:51:12 -0500

    feat: add shadcn components and folder struct

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 sm:items-start">
+    <div className="flex flex-1 flex-col items-center justify-center">
+      <main className="flex w-full max-w-3xl flex-1 flex-col items-center justify-between px-16 py-32 sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10">
+          <h1 className="max-w-xs font-semibold text-3xl leading-10">
             To get started, edit the page.tsx file.
           </h1>
           <p className="max-w-md text-lg leading-8">
