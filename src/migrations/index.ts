e398b44134e1bc1f7aee4fd566f0dd6f9e5dbab5
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-06-08 10:57:03 -0500

    chore: create migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260608_155128 from './20260608_155128';
+
+export const migrations = [
+  {
+    up: migration_20260608_155128.up,
+    down: migration_20260608_155128.down,
+    name: '20260608_155128'
+  },
+];
