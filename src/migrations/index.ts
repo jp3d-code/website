b099051ce8973f80435b859d6c93af0119be6ece
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-07-13 15:32:06 -0500

    feat: add media sizes fields and migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260611_010637 from './20260611_010637';
 import * as migration_20260615_162547 from './20260615_162547';
+import * as migration_20260713_202442 from './20260713_202442';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260615_162547.up,
     down: migration_20260615_162547.down,
-    name: '20260615_162547'
+    name: '20260615_162547',
+  },
+  {
+    up: migration_20260713_202442.up,
+    down: migration_20260713_202442.down,
+    name: '20260713_202442'
   },
 ];
--- src/payload-types.ts
+++ src/payload-types.ts
@@ -169,6 +169,8 @@ export interface User {
   collection: 'users';
 }
 /**
+ * Images and documents used across the site
+ *
  * This interface was referenced by `Config`'s JSON-Schema
  * via the `definition` "media".
  */
@@ -186,6 +188,16 @@ export interface Media {
   height?: number | null;
   focalX?: number | null;
   focalY?: number | null;
+  sizes?: {
+    hero?: {
+      url?: string | null;
+      width?: number | null;
+      height?: number | null;
+      mimeType?: string | null;
+      filesize?: number | null;
+      filename?: string | null;
+    };
+  };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -559,6 +571,20 @@ export interface MediaSelect<T extends boolean = true> {
   height?: T;
   focalX?: T;
   focalY?: T;
+  sizes?:
+    | T
+    | {
+        hero?:
+          | T
+          | {
+              url?: T;
+              width?: T;
+              height?: T;
+              mimeType?: T;
+              filesize?: T;
+              filename?: T;
+            };
+      };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
