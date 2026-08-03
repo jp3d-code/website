de394de2fc9bdd75cf84ba5f1295603bd82e9757
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2026-08-03 17:56:01 -0500

    Disable TeamSection on Sobre Nosotros page

--- src/modules/sobre-nosotros/components/pages/sobre-nosotros-page.tsx
+++ src/modules/sobre-nosotros/components/pages/sobre-nosotros-page.tsx
@@ -1,6 +1,6 @@
 import { HistorySection } from "@/modules/sobre-nosotros/components/sections/history-section";
 import { IntroSection } from "@/modules/sobre-nosotros/components/sections/intro-section";
-import { TeamSection } from "@/modules/sobre-nosotros/components/sections/team-section";
+// import { TeamSection } from "@/modules/sobre-nosotros/components/sections/team-section";
 import { TestimonialsSection } from "@/modules/sobre-nosotros/components/sections/testimonials-section";
 import { VideosSection } from "@/modules/sobre-nosotros/components/sections/videos-section";
 
@@ -8,7 +8,7 @@ export default function SobreNosotrosPage() {
   return (
     <>
       <IntroSection />
-      <TeamSection />
+      {/*<TeamSection />*/}
       <TestimonialsSection />
       <HistorySection />
       <VideosSection />
